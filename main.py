7e27707d4edef743dfe9b08847c96655e54c140c
杨旋 <yangxuan@coldlake.cn>
2021-09-02 17:31:58 +0800

    代码优化

--- main.py
+++ main.py
@@ -11,51 +11,41 @@ import json
 
 eighty_url = 'https://www.1680s.com'
 
-
-def concatUrl(url1, url2):
-    splits = re.split(r'/+',url1)
-    url = splits[0] + '//'
-    if url2.startswith('/'):
-        url = url + splits[1] + url2
-    else:
-        url = url + '/'.join(splits[1:-1]) + '/' + url2
-    return url
-
 class eightysplugin(StellarPlayer.IStellarPlayerPlugin):
     def __init__(self,player:StellarPlayer.IStellarPlayer):
         super().__init__(player)
-        self.menuall = ['最新更新','电影','电视剧','动漫','综艺','体育']
-        self.menudianying = ['动作片','喜剧片','爱情片','科幻片','恐怖片','剧情片','战争片','记录片','动画电影']
-        self.menudianshiju = ['国产剧','欧美剧','日韩剧','港台剧','海外剧']
-        self.menudongman = ['国产','日本','欧美','其它']
-        self.menuzongyi = ['内地','港台','日韩','欧美']
-        self.menutiyu = ['mma','wwe','ufc','足球']
-        self.menu1 = self.menuall[0]
-        self.menu2 = ''
-        self.gridmenu = []
+        self.menuall = []
+        self.secmenu = []
         self.medias = []
-        self.playurl = ''
-        self.searchtype = False
         self.firstpage = ''
         self.lastpage = ''
         self.nextpage = ''
         self.previouspage = ''
         self.cur_page = ''
         self.allmovidesdata = {}
-        searchurl = eighty_url + '/map.html'
-        self.reloadPage(searchurl)
 
+    
+    def start(self):
+        super().start()
+        res = requests.get(eighty_url,verify=False)
+        if res.status_code == 200:
+            bs = bs4.BeautifulSoup(res.content.decode('UTF-8','ignore'),'html.parser')
+            selector = bs.find_all('a', class_='fed-navs-title fed-font-xvi fed-hide-xs fed-hide-sm fed-show-md-block')
+            for item in selector:
+                menuname = item.string
+                menuurl = eighty_url + item.get('href')
+                self.menuall.append({'title':menuname,'url':menuurl}) 
+        searchurl = eighty_url + '/map.html'
+        self.loadMedias(searchurl)
     
     def show(self):
         controls = self.makeLayout()
         self.doModal('main',800,600,'',controls)
-        print('===================2')
     
-    def makeLayout(self):
+    def makeLayout(self):   
         mainmenu = []
         for cat in self.menuall:
-            mainmenu.append({'type':'link','name':cat,'@click':'onMenuMainClick'})
-        secmenu = []
+            mainmenu.append({'type':'link','name':cat['title'],'@click':'onMenuMainClick','width':60})
         
         mediagrid_layout = [
             [
@@ -105,168 +95,111 @@ class eightysplugin(StellarPlayer.IStellarPlayerPlugin):
         return controls
     
     def onSearch(self, *args):
-        self.menu2 = '搜索'
-        self.search_word = self.player.getControlValue('main','search_edit')
-        self.loadMediaList(1)
+        search_word = self.player.getControlValue('main','search_edit')
+        if len(search_word) > 0:
+            searchurl = eighty_url + '/vodsearch/' + urllib.parse.quote(search_word,encoding='utf-8') + '-1.html'
+            self.loadMedias(searchurl)
     
     def onMenuMainClick(self,pageId,control,*args):
         controls = []
+        self.secmenu = []
         self.player.removeControl('main','canremovemenugroup')
-        if control == '最新更新' or control == '动漫' or control == '综艺':
-            self.menu2 = control
-            self.loadMediaList(0)
-        
-        if control == '电影':
-            for cat in self.menudianying:
-                controls.append({'type':'link','name':cat,'width':60,'@click':'onSecondMenuClick'})
-        elif control == '电视剧':
-            self.menu2 = '最新剧集'
-            for cat in self.menudianshiju:
-                controls.append({'type':'link','name':cat,'width':60,'@click':'onSecondMenuClick'})     
-        elif control == '体育':
-            self.menu2 = '最近更新'
-            for cat in self.menutiyu:
-                controls.append({'type':'link','name':cat,'width':60,'@click':'onSecondMenuClick'})                 
-        print(controls)
+        secmenuurl = ''
+        for item in self.menuall:
+            if item['title'] == control:
+                secmenuurl = item['url']
+        if secmenuurl != '':
+            res = requests.get(secmenuurl,verify=False)
+            if res.status_code == 200:
+                bs = bs4.BeautifulSoup(res.content.decode('UTF-8','ignore'),'html.parser')
+                selector = bs.find_all('div', class_='fed-list-head fed-part-rows fed-padding')
+                for item in selector:
+                    nameinfo = item.select('h2')
+                    urlinfo = item.select('ul > li > a')
+                    if nameinfo and urlinfo:
+                        name = nameinfo[0].string
+                        url = eighty_url + urlinfo[0].get('href')
+                        self.secmenu.append({'title':name,'url':url})
+            self.loadMedias(secmenuurl) 
+        for cat in self.secmenu:
+            controls.append({'type':'link','name':cat['title'],'width':60,'@click':'onSecondMenuClick'})
         row = {'group':controls,'name':'canremovemenugroup'}
         self.player.addControl('main','secmenugroup',row)
           
     def onSecondMenuClick(self,pageId,control,*args):
-        self.menu2 = control
-        self.loadMediaList(1)
-        
-    def loadMediaList(self,pageindex):
-        searchurl = ''
-        self.searchtype = False
-        if self.menu2 == '搜索':
-            self.searchtype = True
-            search_word = self.player.getControlValue('main','search_edit')
-            if len(search_word) > 0:
-                searchurl = eighty_url + '/vodsearch/' + urllib.parse.quote(search_word,encoding='utf-8') + '-' + str(pageindex) + '.html'
-        elif self.menu2 == '最新更新':
-            searchurl = eighty_url + '/map.html'
-        elif self.menu2 == '动作片':
-            searchurl = eighty_url + '/vodtype/dongzuopian-' + str(pageindex) + '.html'
-        elif self.menu2 == '喜剧片':
-            searchurl = eighty_url + '/vodtype/xijupian-' + str(pageindex) + '.html'
-        elif self.menu2 == '爱情片':
-            searchurl = eighty_url + '/vodtype/aiqingpian-' + str(pageindex) + '.html'
-        elif self.menu2 == '科幻片':
-            searchurl = eighty_url + '/vodtype/kehuanpian-' + str(pageindex) + '.html'
-        elif self.menu2 == '恐怖片':
-            searchurl = eighty_url + '/vodtype/kongbupian-' + str(pageindex) + '.html'
-        elif self.menu2 == '剧情片':
-            searchurl = eighty_url + '/vodtype/juqingpian-' + str(pageindex) + '.html'
-        elif self.menu2 == '战争片':
-            searchurl = eighty_url + '/vodtype/zhanzhengpian-' + str(pageindex) + '.html'
-        elif self.menu2 == '记录片':
-            searchurl = eighty_url + '/vodtype/jilupian-' + str(pageindex) + '.html'
-        elif self.menu2 == '动画电影':
-            searchurl = eighty_url + '/vodtype/donghua-' + str(pageindex) + '.html'
-        elif self.menu2 == '国产剧':
-            searchurl = eighty_url + '/vodtype/guochanju-' + str(pageindex) + '.html'
-        elif self.menu2 == '欧美剧':
-            searchurl = eighty_url + '/vodtype/oumeiju-' + str(pageindex) + '.html'
-        elif self.menu2 == '日韩剧':
-            searchurl = eighty_url + '/vodtype/rihanju-' + str(pageindex) + '.html'
-        elif self.menu2 == '港台剧':
-            searchurl = eighty_url + '/vodtype/gangtaiju-' + str(pageindex) + '.html'
-        elif self.menu2 == '海外剧':
-            searchurl = eighty_url + '/vodtype/haiwaiju-' + str(pageindex) + '.html'
-        elif self.menu2 == '动漫':
-            searchurl = eighty_url + '/vodtype/dongman-' + str(pageindex) + '.html'
-        elif self.menu2 == '综艺':
-            searchurl = eighty_url + '/vodtype/zongyi-' + str(pageindex) + '.html'
-        elif self.menu2 == 'mma':
-            searchurl = eighty_url + '/vodtype/mma-' + str(pageindex) + '.html'
-        elif self.menu2 == 'wwe':
-            searchurl = eighty_url + '/vodtype/wwe-' + str(pageindex) + '.html'
-        elif self.menu2 == 'ufc':
-            searchurl = eighty_url + '/vodtype/ufc-' + str(pageindex) + '.html'
-        elif self.menu2 == '足球':
-            searchurl = eighty_url + '/vodtype/zhuqiu-' + str(pageindex) + '.html'
-        print(searchurl)
-        self.reloadPage(searchurl)
-        
+        selecturl = ''
+        for item in self.secmenu:
+            if item['title'] == control:
+                selecturl = item['url']
+        print(selecturl)
+        if selecturl != '':
+            self.loadMedias(selecturl)
+    
+    def loadMedias(self,url):
+        self.loading()
+        self.medias = []
+        res = requests.get(url,verify=False)
+        if res.status_code == 200:
+            bs = bs4.BeautifulSoup(res.content.decode('UTF-8','ignore'),'html.parser')
+            selector = bs.find_all('li', class_='fed-list-item fed-padding fed-col-xs4 fed-col-sm3 fed-col-md2')
+            if selector:
+                for item in selector:
+                    picinfo = item.select('a.fed-list-pics.fed-lazy.fed-part-2by3')
+                    urlinfo = item.select('a.fed-list-title.fed-font-xiv.fed-text-center.fed-text-sm-left.fed-visible.fed-part-eone')
+                    if picinfo and urlinfo:
+                        pic = eighty_url + picinfo[0].get('data-original')
+                        name = urlinfo[0].string
+                        mediaurl = eighty_url + urlinfo[0].get('href')
+                        self.medias.append({'url':mediaurl,'picture':pic,'title':name})
+            else:
+                selector = bs.find_all('dl', class_='fed-list-deta fed-deta-padding fed-line-top fed-margin fed-part-rows fed-part-over')
+                if selector:
+                    for item in selector:
+                        picinfo = item.select('dt > a')
+                        urlinfo = item.select('dd.fed-deta-content.fed-col-xs7.fed-col-sm8 > h3 > a')
+                        if picinfo and urlinfo:
+                            pic = eighty_url + picinfo[0].get('data-original')
+                            name = urlinfo[0].string
+                            mediaurl = eighty_url + urlinfo[0].get('href')
+                            self.medias.append({'url':mediaurl,'picture':pic,'title':name})
+            pageselector = bs.find_all('div',class_='fed-page-info fed-text-center')
+            if pageselector:
+                pages = pageselector[0].select('a')
+                n = len(pages)
+                self.firstpage =  eighty_url + pages[0].get('href')
+                self.lastpage = eighty_url + pages[n - 1].get('href')
+                self.nextpage = eighty_url + pages[n - 2].get('href')
+                self.previouspage = eighty_url + pages[1].get('href')
+                self.cur_page = '第' + pages[n - 3].getText() + '页'
+                print(self.firstpage)
+                print(self.nextpage)
+                print(self.lastpage)
+                print(self.previouspage)
+            else:
+                self.cur_page = '第1/1页'
+        self.player.updateControlValue('main','mediagrid',self.medias)
+        self.loading(True)
+               
     def onClickFirstPage(self, *args):
         if self.firstpage == '':
             return 
-        self.reloadPage(self.firstpage)
+        self.loadMedias(self.firstpage)
         
     def onClickFormerPage(self, *args):
         if self.previouspage == '':
             return 
-        self.reloadPage(self.previouspage)
+        self.loadMedias(self.previouspage)
     
     def onClickNextPage(self, *args):
         if self.nextpage == '':
             return 
-        self.reloadPage(self.nextpage)
+        self.loadMedias(self.nextpage)
         
     def onClickLastPage(self, *args):
         if self.lastpage == '':
             return 
-        self.reloadPage(self.lastpage)
-        
-    def reloadPage(self,url):    
-        medialist = []
-        self.firstpage = ''
-        self.lastpage = ''
-        self.nextpage = ''
-        self.previouspage = ''
-        self.loading()
-        if url != '':
-            res = requests.get(url,verify=False)
-            if res.status_code == 200:         
-                bs = bs4.BeautifulSoup(res.content.decode('UTF-8','ignore'),'html.parser')
-                if self.searchtype:
-                    selector = bs.find_all('dd', class_='fed-deta-content fed-col-xs7 fed-col-sm8')
-                    for item in selector:
-                        urlinfo = item.parent.select('dd.fed-deta-button.fed-col-xs7.fed-col-sm8.fed-part-rows > a')[0]
-                        picinfo = item.parent.select('dt.fed-deta-images.fed-list-info.fed-col-xs3 > a')[0]
-                        nameinfo = item.select('h3.fed-part-eone')[0].select('a')[0]
-                        url = eighty_url + urlinfo.get('href')
-                        pic = eighty_url + picinfo.get('data-original')
-                        name = nameinfo.getText()
-                        medialist.append({'url':url,'picture':pic,'title':name})
-                        
-                    pageselector = bs.find_all('div',class_='fed-page-info fed-text-center')
-                    if pageselector:
-                        pages = pageselector[0].select('a')
-                        n = len(pages)
-                        self.firstpage =  eighty_url + pages[0].get('href')
-                        self.lastpage = eighty_url + pages[n - 1].get('href')
-                        self.nextpage = eighty_url + pages[n - 2].get('href')
-                        self.previouspage = eighty_url + pages[1].get('href')
-                        self.cur_page = '第' + pages[n - 3].getText() + '页'
-                    else:
-                        self.cur_page = '第1/1页'
-                else:
-                    selector = bs.select('body > div.fed-main-info.fed-min-width > div > div.fed-list-new.fed-part-layout.fed-back-whits > ul > li')
-                    for item in selector:
-                        urlinfo = item.select('a.fed-list-pics.fed-lazy.fed-part-2by3')[0]
-                        nameinfo = item.select('a.fed-list-title.fed-font-xiv.fed-text-center.fed-text-sm-left.fed-visible.fed-part-eone')[0]
-                        url = eighty_url + urlinfo.get('href')
-                        pic = eighty_url + urlinfo.get('data-original')
-                        name = nameinfo.getText()
-                        medialist.append({'url':url,'picture':pic,'title':name})
-                        
-                    pageselector = bs.find_all('div',class_='fed-page-info fed-text-center')
-                    if pageselector:
-                        pages = pageselector[0].select('a')
-                        n = len(pages)
-                        self.firstpage =  eighty_url + pages[0].get('href')
-                        self.lastpage = eighty_url + pages[n - 1].get('href')
-                        self.nextpage = eighty_url + pages[n - 2].get('href')
-                        self.previouspage = eighty_url + pages[1].get('href')
-                        self.cur_page = '第' + pages[n - 3].getText() + '页'
-                    else:
-                        self.cur_page = '第1/1页'
-                    
-        self.medias = medialist
-        self.player.updateControlValue('main','mediagrid',self.medias)
-        self.loading(True)
-        
+        self.loadMedias(self.lastpage)
+     
     def getPlayPageUrl(self,pageurl):
         res = requests.get(pageurl,verify=False)
         if res.status_code == 200:
@@ -351,12 +284,12 @@ class eightysplugin(StellarPlayer.IStellarPlayerPlugin):
             },
             {'type':'space','height':5},
             {'group':
-                {'type':'grid','name':'movielist','itemlayout':movie_list_layout,'value':actmovies,'separator':True,'itemheight':25,'itemwidth':60},
-                'height':150
+                {'type':'grid','name':'movielist','itemlayout':movie_list_layout,'value':actmovies,'separator':True,'itemheight':25,'itemwidth':120},
+                'height':200
             }
         ]
         self.loading(True)
-        self.player.doModal(medianame,400,420,medianame,controls)
+        self.player.doModal(medianame,400,470,medianame,controls)
 
     
     def on_xl_click(self, page, listControl, item, itemControl):
@@ -378,10 +311,12 @@ class eightysplugin(StellarPlayer.IStellarPlayerPlugin):
                 selector = bs.find_all('div', class_='fed-play-player')[0]
                 item = selector.select('div')[0]
                 scriptitem = item.select('script')[0]
-                jsonstr = re.findall(r"var player_data=(.+)",scriptitem.string)[0]
+                print(scriptitem)
+                jsonstr = re.findall(r"var player_aaaa=(.+)",scriptitem.string)[0]
                 playerjson = json.loads(jsonstr)
-                self.playurl  = playerjson['url']
-                self.player.play(self.playurl)
+                print(playerjson)
+                playurl  = playerjson['url']
+                self.player.play(playurl)
             except:
                 selector = bs.find_all('p', class_='copyright_notice')
                 if len(selector) > 0:
